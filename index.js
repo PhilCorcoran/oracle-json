fcea144d1e663bf0871fef5b2b7cd10543f0a685
PhilCorcoran <corcoranp@LIPC-01737.laya.ie>
2013-12-21 08:59:22 +0000

    Always close connection after executing query

--- index.js
+++ index.js
@@ -5,51 +5,19 @@ execute_only expects no result
 Some procedures accept a JSON input parameter while others take no parameter
 */
 var oracle=require('oracle');
-var connection=null;
 var dbSettings=null;
 var lastOracleError="";
 var aSecret="someSecret";
+
 function init(settings){
         dbSettings=settings.database;
         aSecret=settings.secret;
 }
-function connect(successFunc,errorFunc){
-        lastOracleError="";
-        if(dbSettings===null){
-                console.log("init() was never called "); 
-                errorFunc();              
-        }
-        if(isConnected()){
-                console.log("Already connected when connect() was called");
-                successFunc();
-        }
-        console.log("Connecting to Oracle server:"+dbSettings.hostname);
-        oracle.connect(dbSettings,function(err, ora_connection) {
-                if(err){
-                        console.log("Error: Could not connect to Oracle."+err);
-                        lastOracleError=(new Date()).valueOf()+":"+err;
-                        close();
-                        errorFunc();
-                }else{
-                        connection=ora_connection;
-                        successFunc();
-                }
-        });
-}
 
-function isConnected(){
-        if(connection===null){
-                return false;
-        }
-        if(!connection.isConnected()){
-                return false;
-        }
-        return true;
-}
 /*  Execute a procedure which returns a JSON clob 
         dbRequest={procedure,params,response,successFunc} 
         */
-        function execute(dbRequest){
+        function execute(dbRequest,connection){
                 if(dbRequest.params!==undefined){
                         var callString='call '+dbRequest.procedure+'(:1,:2)';
                         var paramString=JSON.stringify(dbRequest.params);
@@ -61,10 +29,10 @@ function isConnected(){
                         connection.execute(callString, [new oracle.OutParam(oracle.OCCICLOB)],handle_result_err);
                 }
                 function handle_result_err(err,results){
+                        close(connection);
                         if (err) {
                                 console.log("Oracle Error(1): "+err);
                                 lastOracleError=(new Date()).valueOf()+":"+err;
-                                close();
                                 dbRequest.response.status(503).send("Database Error");
                         } else if(!results || ! results.returnParam) {
                                 console.log("Oracle Error(2): no results from database");
@@ -89,22 +57,17 @@ function isConnected(){
                 lastOracleError="";
                 var dbRequest=dbRequest;
                 validate(dbRequest);
-                if(!isConnected()){
-                        console.log("Trying to connect to Oracle");
-                        oracle.connect(dbSettings,function(err, ora_connection) {
-                                if(err){
-                                        console.log("Error: Could not connect to Oracle."+err);
-                                        lastOracleError=(new Date()).valueOf()+":"+err;
-                                        close();
-                                        dbRequest.response.status(503).send("Database Error");
-                                }else{
-                                        connection=ora_connection;
-                                        execute(dbRequest);
-                                }
-                        });
-                }else{
-                        execute(dbRequest);
-                }
+                console.log("Trying to connect to Oracle");
+                oracle.connect(dbSettings,function(err, connection) {
+                        if(err){
+                                console.log("Error: Could not connect to Oracle."+err);
+                                lastOracleError=(new Date()).valueOf()+":"+err;
+                                close(connection);
+                                dbRequest.response.status(503).send("Database Error");
+                        }else{
+                                execute(dbRequest,connection);
+                        }
+                });
         }
         function connectExecuteOnly(dbRequest){
                 lastOracleError="";
@@ -118,25 +81,37 @@ function isConnected(){
                         console.log("Error. procedure is not defined");
                         dbRequest.response.status(503).send("Database Error");
                 }
-                if(!isConnected()){
-                        console.log("Trying to connect to Oracle");
-                        oracle.connect(dbSettings,function(err, ora_connection) {
-                                if(err){
-                                        console.log("Oracle Error(3): Could not connect to Oracle."+err);
-                                        lastOracleError=(new Date()).valueOf()+":"+err;
-                                        close();
-                                        dbRequest.response.status(503).send("Database Error");
-                                }else{
-                                        connection=ora_connection;
-                                        execute_only(dbRequest);
-                                }
-                        });
-                }else{
-                        execute_only(dbRequest);
+                console.log("Trying to connect to Oracle:"+dbSettings);
+                oracle.connect(dbSettings,function(err, connection) {
+                        if(err){
+                                console.log("Oracle Error(3): Could not connect to Oracle"+err);
+                                lastOracleError=(new Date()).valueOf()+":"+err;
+                                close(connection);
+                                dbRequest.response.status(503).send("Database Error");
+                        }else{
+                                execute_only(dbRequest,connection);
+                        }
+                });
+        }
+        function testConnect(successFunc,errorFunc){
+                console.log("Testing connection to Oracle");
+                lastOracleError="";
+                if(dbSettings===null){
+                        console.log("Error. init() was never called");
                 }
+                oracle.connect(dbSettings,function(err, connection) {
+                        close(connection);
+                        if(err){
+                                console.log("Oracle Error(3): Could not connect to Oracle"+err);
+                                lastOracleError=(new Date()).valueOf()+":"+err;
+                                errorFunc();
+                        }else{
+                                successFunc();
+                        }
+                });
         }
         /* Execute a procedure which returns no results */
-        function execute_only(dbRequest){
+        function execute_only(dbRequest,connection){
                 if(dbRequest.params===undefined){
                         connection.execute("call"+dbRequest.procedure+"()", [],handle_result_err);
                 }else{
@@ -146,22 +121,22 @@ function isConnected(){
                         connection.execute(callString, [paramString],handle_result_err);
                 }
                 function handle_result_err(err,results){
+                        close(connection);                        
                         if (err) {
                                 console.log("Oracle Error(4): "+err);
                                 lastOracleError=(new Date()).valueOf()+":"+err;
-                                close();
                                 dbRequest.response.status(503).send("Database Error");
                         }else {
                                 dbRequest.successFunc();
                         }
                 };
         }
-        function close(){
+        function close(connection){
+                console.log("Closing Oracle connection");
                 if(connection !==null){
                         connection.close();
-                        connection=null;
                 }else{
-                        console.log("Error: close() called but connection was already null");
+                        console.log("Error: close() called but connection was null");
                 }
         }
         function lastError(){
@@ -189,8 +164,7 @@ function isConnected(){
         exports.connectExecute=connectExecute;
         exports.connectExecuteOnly=connectExecuteOnly;
         exports.init=init;
-        exports.close=close;
-        exports.connect=connect;
+        exports.testConnect=testConnect;
         exports.lastError=lastError;
         exports.admin=admin;
 
